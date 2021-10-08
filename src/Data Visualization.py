542d4667a3da0f54f8447b64e247ec17e46147ee
MatiasRicardoMuenteCastro <matiascastro@outlook.com.br>
2021-10-08 01:35:05 -0300

    Added new pallet for graphs

--- src/Data Visualization.py
+++ src/Data Visualization.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 #Plota o gráfico dos saldos de cada faixa etária
 def AgeWithBalance(Bank_Data):
     custom_style = {'axes.labelcolor': 'black',
-                    'xtick.color': '#3b157e',
+                    'xtick.color': 'black',
                     'ytick.color': '#3b157e'}
     new_style = {'grid': False}
+
     Bank_Data = Bank_Data.sort_values('age')
-    #divergning = sns.diverging_palette(250, 15, s=75, l=40,n=9, center= "dark")
     plt.figure(figsize = (13,8))
     plt.style.use("ggplot")
     plt.rc('axes', **new_style)
@@ -22,9 +22,16 @@ def AgeWithBalance(Bank_Data):
 
 #Plota o gráfico da duração de chamada por idade
 def AgeWithDuration(Bank_Data):
+    custom_style = {'axes.labelcolor': 'black',
+                    'xtick.color': 'black',
+                    'ytick.color': '#FA2B5D'}
+    new_style = {'grid': False}
+
     Bank_Data = Bank_Data.sort_values('duration',ascending = False)
     plt.figure(figsize = (13,8))
     plt.style.use("ggplot")
+    plt.rc('axes', **new_style)
+    sns.set_style(rc=custom_style)
     plt.title("Tempo médio de duração de uma ligação de marketing por faixa etária (Segundos)")
     sns.lineplot(x = 'age', y = 'duration', data = Bank_Data,alpha = 0.75)
     plt.xlabel('Idade')
@@ -32,6 +39,11 @@ def AgeWithDuration(Bank_Data):
     plt.show(block = True)
 
 def ClientsQuantityAge(Bank_Data):
+    custom_style = {'axes.labelcolor': 'black',
+                    'xtick.color': '#3b157e',
+                    'ytick.color': '#3b157e'}
+    new_style = {'grid': False}
+
     AgesCount = pd.DataFrame(pd.value_counts(Bank_Data['age']))
     AgesDF = pd.DataFrame({"age": AgesCount.index,"quantity" : AgesCount['age']})
     AgesDF = AgesDF.reset_index(drop = True)
@@ -39,22 +51,36 @@ def ClientsQuantityAge(Bank_Data):
     figure = plt.figure(figsize=(18,6))
     figure.set_size_inches(30, 6)
     plt.style.use("ggplot")
+    plt.rc('axes', **new_style)
+    sns.set_style(rc=custom_style)
     plt.title("Faixa etária dos clientes do banco")
-    sns.barplot(x = 'age',y = 'quantity', data = AgesDF, alpha = 0.75)
+    sns.barplot(x = 'age',y = 'quantity', data = AgesDF, palette = "magma", alpha = 0.75)
     plt.ylabel("Número de clientes")
     plt.xlabel("Idades")
     plt.show(block = True)
 
 def AgeMarital(Bank_Data):
+    custom_style = {'axes.labelcolor': 'black',
+                    'xtick.color': 'black',
+                    'ytick.color': '#3b157e'}
+    new_style = {'grid': False}
+
     plt.figure(figsize = (13,7))
     plt.style.use("ggplot")
+    plt.rc('axes', **new_style)
+    sns.set_style(rc=custom_style)
     plt.title("Estado civil dos clientes da instituição financeira")
-    sns.stripplot(x = 'marital', y = 'age', data = Bank_Data)
+    sns.stripplot(x = 'marital', y = 'age', data = Bank_Data,alpha = 0.75,palette = "flare")
     plt.xlabel("Estado civil")
     plt.ylabel("Idade")
     plt.show(block = True)
 
 def JobsQuanity(Bank_Data):
+    custom_style = {'axes.labelcolor': 'black',
+                    'xtick.color': '#3b157e',
+                    'ytick.color': '#3b157e'}
+    new_style = {'grid': False}
+
     JobValues = pd.value_counts(Bank_Data["job"])
     JobDF = pd.DataFrame({"quantity": JobValues})
     JobDF = JobDF.reset_index()
@@ -63,17 +89,26 @@ def JobsQuanity(Bank_Data):
 
     plt.figure(figsize=(15,6))
     plt.style.use("ggplot")
+    plt.rc('axes', **new_style)
+    sns.set_style(rc=custom_style)
     plt.title("Quantidade de pessoas que estão em determinados empregos")
-    sns.barplot(x = 'index', y = 'quantity', data = JobDF,alpha = 0.75)
+    sns.barplot(x = 'index', y = 'quantity', data = JobDF,alpha = 0.75, palette = "magma")
     plt.xlabel("Empregos")
     plt.ylabel("Quantidade de clientes")
     plt.show(block = True)
 
 def BalanceWithJob(Bank_Data):
+    custom_style = {'axes.labelcolor': 'black',
+                    'xtick.color': 'black',
+                    'ytick.color': '#3b157e'}
+    new_style = {'grid': False}
+
     plt.figure(figsize=(15, 6))
     plt.style.use("ggplot")
+    plt.rc('axes', **new_style)
+    sns.set_style(rc=custom_style)
     plt.title("Saldo médio por ano de cada profissão em euros")
-    sns.stripplot(x = 'job', y = 'balance',data = Bank_Data)
+    sns.stripplot(x = 'job', y = 'balance',data = Bank_Data, alpha = 0.75, palette = "flare")
     plt.xlabel("Empregos")
     plt.ylabel("Saldo médio")
     plt.show(block = True)
@@ -89,13 +124,17 @@ def AgeWithLoan(Bank_Data):
     plt.figure(figsize = (15,6))
     plt.title("O volume das idades que têm algum emprestimo")
     plt.style.use("ggplot")
-    sns.countplot(y = 'Age     Loan',data = AgeLoanDF)
+    sns.countplot(y = 'Age     Loan',data = AgeLoanDF, palette = "magma", alpha = 0.75)
     plt.xlabel("Quantidades de clientes")
     plt.ylabel("Grupos das idades dos clientes")
     print(AgeLoanDF.value_counts())
     plt.show(block = True)
 
 def AgeWithHousing(Bank_Data):
+    custom_style = {'axes.labelcolor': 'black',
+                    'xtick.color': '#3b157e',
+                    'ytick.color': '#3b157e'}
+    new_style = {'grid': False}
     Bank_Data = Bank_Data.sort_values('age')
     tempArray = []
     for line in list(range(Bank_Data['age_agroupment'].count())):
@@ -105,14 +144,20 @@ def AgeWithHousing(Bank_Data):
 
     plt.figure(figsize=(15, 6))
     plt.style.use("ggplot")
+    plt.rc('axes', **new_style)
+    sns.set_style(rc=custom_style)
     plt.title("O volume das idades que têm financiamento de uma casa")
-    sns.countplot(y = 'Age Housing',data = AgeHousingDF)
+    sns.countplot(y = 'Age Housing',data = AgeHousingDF, palette = "magma", alpha = 0.75)
     plt.xlabel("Quantidade de clientes")
     plt.ylabel("Grupos das idades dos clientes")
     print(AgeHousingDF.value_counts())
     plt.show(block=True)
 
 def AgeWithDefault(Bank_Data):
+    custom_style = {'axes.labelcolor': 'black',
+                    'xtick.color': '#3b157e',
+                    'ytick.color': '#3b157e'}
+    new_style = {'grid': False}
     Bank_Data = Bank_Data.sort_values('age')
     tempArray = []
     for line in list(range(Bank_Data['age_agroupment'].count())):
@@ -122,23 +167,36 @@ def AgeWithDefault(Bank_Data):
 
     plt.figure(figsize=(15, 6))
     plt.style.use("ggplot")
+    plt.rc('axes', **new_style)
+    sns.set_style(rc=custom_style)
     plt.title("O volume das idades que estão com o nome sujo")
-    sns.countplot(y = 'Age Default',data = AgeDefaultDF)
+    sns.countplot(y = 'Age Default',data = AgeDefaultDF, alpha = 0.75, palette = "magma")
     plt.xlabel("Quantidade de clientes")
     plt.ylabel("Grupos das idades dos clientes")
     print(AgeDefaultDF.value_counts())
     plt.show(block=True)
 
 def ContactWithDuration(Bank_Data):
+    custom_style = {'axes.labelcolor': 'black',
+                    'xtick.color': 'black',
+                    'ytick.color': '#3b157e'}
+    new_style = {'grid': False}
+
     plt.figure(figsize = (15,6))
     plt.style.use("ggplot")
+    plt.rc('axes', **new_style)
+    sns.set_style(rc=custom_style)
     plt.title("Meio de contato que os clientes ficam conversando por mais tempo")
-    sns.stripplot(x = 'contact', y = 'duration', data = Bank_Data)
+    sns.stripplot(x = 'contact', y = 'duration', data = Bank_Data, alpha = 0.75, palette = "flare")
     plt.xlabel('Tipo de contato')
     plt.ylabel('Duração do contato')
     plt.show(block = True)
 
 def ContactWithAge(Bank_Data):
+    custom_style = {'axes.labelcolor': 'black',
+                    'xtick.color': '#3b157e',
+                    'ytick.color': '#3b157e'}
+    new_style = {'grid': False}
     Bank_Data = Bank_Data.sort_values('age')
     tempArray = []
     for line in list(range(Bank_Data['age_agroupment'].count())):
@@ -148,13 +206,19 @@ def ContactWithAge(Bank_Data):
 
     plt.figure(figsize=(15, 6))
     plt.style.use("ggplot")
+    plt.rc('axes', **new_style)
+    sns.set_style(rc=custom_style)
     plt.title("Idades que mais utilizam determinados meios de contatos")
-    sns.countplot(y='Age Contact', data=AgeContactDF)
+    sns.countplot(y='Age Contact', data=AgeContactDF, palette = "magma", alpha = 0.75)
     plt.ylabel('Groupo Etário / Tipo de contato')
     plt.xlabel('Quantidade de pessoas')
     plt.show(block = True)
 
 def StatusCampaigns(Bank_Data):
+    custom_style = {'axes.labelcolor': 'black',
+                    'xtick.color': 'black',
+                    'ytick.color': '#3b157e'}
+    new_style = {'grid': False}
     TempArray = []
     line = 0
     for item in Bank_Data['poutcome']:
@@ -166,8 +230,10 @@ def StatusCampaigns(Bank_Data):
 
     plt.figure(figsize=(10, 6))
     plt.style.use("ggplot")
+    plt.rc('axes', **new_style)
+    sns.set_style(rc=custom_style)
     plt.title("Status das campanhas de marketing até agora feitas")
-    sns.countplot(x = 'Status',data = StatusDF)
+    sns.countplot(x = 'Status',data = StatusDF, alpha = 0.75, palette = "magma")
     plt.xlabel('Status da campanha')
     plt.ylabel('Quantidades de falha ou sucesso')
     plt.show(block=True)
@@ -181,7 +247,7 @@ Bank_Data = pd.read_excel(file_path)
 #AgeMarital(Bank_Data)
 #JobsQuanity(Bank_Data)
 #BalanceWithJob(Bank_Data)
-#AgeWithLoan(Bank_Data)
+AgeWithLoan(Bank_Data)
 #AgeWithHousing(Bank_Data)
 #AgeWithDefault(Bank_Data)
 #ContactWithDuration(Bank_Data)
--- src/DataVisualizationSecondary.py
+++ src/DataVisualizationSecondary.py
@@ -42,22 +42,34 @@ def OrganizeAscending(Bank_Data,job):
     return BalanceDF
 
 def BalanceWithBlueCollar(Bank_Data):
+    custom_style = {'axes.labelcolor': 'black',
+                    'xtick.color': '#3b157e',
+                    'ytick.color': '#3b157e'}
+    new_style = {'grid': False}
     BlueCollarDF = OrganizeAscending(Bank_Data,'blue-collar')
     plt.figure(figsize=(15, 6))
     plt.style.use("ggplot")
+    plt.rc('axes', **new_style)
+    sns.set_style(rc=custom_style)
     plt.title("Saldo dos clientes que são trabalhadores de colarinho azul (euros)")
-    sns.barplot(x='balance_agroupment', y='quantity', data=BlueCollarDF, alpha = 0.75)
+    sns.barplot(x='balance_agroupment', y='quantity', data=BlueCollarDF, alpha = 0.75, palette = "magma")
     plt.xlabel("Faixa de saldo")
     plt.ylabel("Quantidade de clientes")
     BlueCollarCount = BlueCollarDF.value_counts()
     plt.show(block=True)
 
 def BalanceWithRetired(Bank_Data):
+    custom_style = {'axes.labelcolor': 'black',
+                    'xtick.color': '#3b157e',
+                    'ytick.color': '#3b157e'}
+    new_style = {'grid': False}
     RetiredDF = OrganizeAscending(Bank_Data, 'retired')
     plt.figure(figsize=(17, 6))
     plt.style.use("ggplot")
+    plt.rc('axes', **new_style)
+    sns.set_style(rc=custom_style)
     plt.title("Saldo dos clientes que estão aposentados (euros)")
-    sns.barplot(x='balance_agroupment', y='quantity', data=RetiredDF, alpha = 0.75)
+    sns.barplot(x='balance_agroupment', y='quantity', data=RetiredDF, alpha = 0.75, palette = "magma")
     plt.xlabel("Faixa de saldo")
     plt.ylabel("Quantidade de clientes")
     RetiredCount = RetiredDF.value_counts()
@@ -65,6 +77,10 @@ def BalanceWithRetired(Bank_Data):
     plt.show(block = True)
 
 def BalanceWithManagement(Bank_Data):
+    custom_style = {'axes.labelcolor': 'black',
+                    'xtick.color': '#3b157e',
+                    'ytick.color': '#3b157e'}
+    new_style = {'grid': False}
     ManagementDF = OrganizeAscending(Bank_Data, 'management')
     group = []
     for item in ManagementDF['balance_agroupment']:
@@ -85,8 +101,10 @@ def BalanceWithManagement(Bank_Data):
     ManagementDF['balance_agroupment'] = groupSeries
     plt.figure(figsize=(20, 6))
     plt.style.use("ggplot")
+    plt.rc('axes', **new_style)
+    sns.set_style(rc=custom_style)
     plt.title("Saldo dos clientes que são gerentes (euros)")
-    sns.barplot(x='balance_agroupment', y='quantity', data=ManagementDF, alpha = 0.75)
+    sns.barplot(x='balance_agroupment', y='quantity', data=ManagementDF, alpha = 0.75, palette = "magma")
     plt.xlabel("Faixa de saldo")
     plt.ylabel("Quantidade de clientes")
     ManagmentCount = ManagementDF.value_counts()
@@ -95,11 +113,17 @@ def BalanceWithManagement(Bank_Data):
 
 
 def BalanceWithTechnician(Bank_Data):
+    custom_style = {'axes.labelcolor': 'black',
+                    'xtick.color': '#3b157e',
+                    'ytick.color': '#3b157e'}
+    new_style = {'grid': False}
     technician = OrganizeAscending(Bank_Data, 'technician')
     plt.figure(figsize=(18, 6))
     plt.style.use("ggplot")
+    plt.rc('axes', **new_style)
+    sns.set_style(rc=custom_style)
     plt.title("Saldo dos clientes que são tecnicos (euros)")
-    sns.barplot(x='balance_agroupment', y='quantity', data=technician, alpha = 0.75)
+    sns.barplot(x='balance_agroupment', y='quantity', data=technician, alpha = 0.75, palette = "magma")
     plt.xlabel("Faixa de saldo")
     plt.ylabel("Quantidade de clientes")
     TechnicianCount = technician.value_counts()
@@ -107,11 +131,17 @@ def BalanceWithTechnician(Bank_Data):
     plt.show(block=True)
 
 def BalanceWithAdmin(Bank_Data):
+    custom_style = {'axes.labelcolor': 'black',
+                    'xtick.color': '#3b157e',
+                    'ytick.color': '#3b157e'}
+    new_style = {'grid': False}
     admins = OrganizeAscending(Bank_Data, 'admin.')
     plt.figure(figsize=(18, 6))
     plt.style.use("ggplot")
+    plt.rc('axes', **new_style)
+    sns.set_style(rc=custom_style)
     plt.title("Saldo dos clientes que são administradores (euros)")
-    sns.barplot(x='balance_agroupment', y='quantity', data=admins,alpha = 0.75)
+    sns.barplot(x='balance_agroupment', y='quantity', data=admins,alpha = 0.75,palette = "magma")
     plt.xlabel("Faixa de saldo")
     plt.ylabel("Quantidade de clientes")
     AdminsCount = admins.value_counts()
@@ -119,11 +149,17 @@ def BalanceWithAdmin(Bank_Data):
     plt.show(block=True)
 
 def BalanceWithServices(Bank_Data):
+    custom_style = {'axes.labelcolor': 'black',
+                    'xtick.color': '#3b157e',
+                    'ytick.color': '#3b157e'}
+    new_style = {'grid': False}
     services = OrganizeAscending(Bank_Data, 'services')
     plt.figure(figsize=(18, 6))
     plt.style.use("ggplot")
+    plt.rc('axes', **new_style)
+    sns.set_style(rc=custom_style)
     plt.title("Saldo dos clientes que trabalham na prestação de serviços (euros)")
-    sns.barplot(x='balance_agroupment', y='quantity', data=services,alpha = 0.75)
+    sns.barplot(x='balance_agroupment', y='quantity', data=services,alpha = 0.75, palette = "magma")
     plt.xlabel("Faixa de saldo")
     plt.ylabel("Quantidade de clientes")
     ServicesCount = services.value_counts()
